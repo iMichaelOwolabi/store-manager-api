e65c2a6fdb83742230b6314d490d011888d0f40d
Michael Owolabi <michael.i.owolabi@gmail.com>
2018-10-21 00:11:21 +0100

    Add users route to the index file

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const homeRoute = require('./home');
+const usersRoute = require('./api/v1/users');
 
 router.use('/', homeRoute);
+router.use('./api/v1/users', usersRoute);
 
 module.exports = router;
