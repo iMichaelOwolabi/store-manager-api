14c79ed8d4d16a76de0279557e0e7dff1d4011c1
Michael Owolabi <michael.i.owolabi@gmail.com>
2018-10-21 00:51:18 +0100

    Add post, put and delete method to products resource

--- src/routes/api/v1/products.js
+++ src/routes/api/v1/products.js
@@ -12,4 +12,70 @@ router.get('/', (req, res)=> {
 	res.status(200).json(inMemoryProducts);
 });
 
+//Getting a apecific product
+router.get('/:id', (req, res)=> {
+    const {id} = req.params;
+    const product = inMemoryProducts.filter((theProduct)=> theProduct.id === parseInt(id));
+
+    if(!product){
+        res.status(404).send(`Product with id ${id} does not exist on this platform`);
+        return;
+    }
+    
+    res.status(200).json(product);
+});
+
+//Creating a new product
+router.post('/', (req, res)=> {
+    const {productName, price, category, quantity} = req.body;
+    const lastId = inMemoryProducts.length;
+    const id = lastId + 1;
+
+    let newProduct = {
+        'id': id,
+        'productName': productName,
+        'price': price,
+        'category': category,
+        'quantity': quantity
+    };
+
+    inMemoryProducts.push(newProduct);
+    res.status(201).location(`./api/products/${id}`).json(newProduct);
+});
+
+//Updatinging existing product
+router.put('/:id', (req, res)=> {
+    const id = req.params.id;
+    const {productName, price, category, quantity} = req.body;
+    const product = inMemoryProducts.filter((theProduct)=> theProduct.id === parseInt(id));
+    if(!product){
+        res.status(404).send(`The product with the given id cannot be found`);
+    }
+
+    const newUpdate = {
+        'id': id, 
+        'productName': productName,
+        'price': price,
+        'category': category,
+        'quantity': quantity
+    }
+    if(!newUpdate){
+        res.status(400).send(`Bad request, kindly check the supplied values`);
+    }
+    res.status(200).location(`./api/products/${id}`).json(newUpdate);
+});
+
+//Delete a specific product
+router.delete('/:id', (req, res)=> {
+    const {id} = req.params;
+    const product = inMemoryProducts.filter((theProduct)=> theProduct.id === parseInt(id));
+
+    if(!product){
+        res.status(404).send(`The product with the given id cannot be found`);
+        return;
+    }
+    
+    res.status(200).send(`The product has been successfully deleted`);
+});
+
 module.exports = router;
