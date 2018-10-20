20c394efe28eb77676f2ef058aea1bbfc04e5a21
Michael Owolabi <michael.i.owolabi@gmail.com>
2018-10-21 00:40:18 +0100

    Create products endpoints with the get method

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const homeRoute = require('./home');
 const usersRoute = require('./api/v1/users');
+const productsRoute = require('./api/v1/products');
 
 router.use('/', homeRoute);
 router.use('/api/v1/users', usersRoute);
+router.use('/api/v1/products', productsRoute);
 
 
 module.exports = router;
