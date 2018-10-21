05faf1aca670661f45c375a13b563dbf2f155dd6
Michael Owolabi <michael.i.owolabi@gmail.com>
2018-10-21 01:02:33 +0100

    Add sales route to index file

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const homeRoute = require('./home');
 const usersRoute = require('./api/v1/users');
 const productsRoute = require('./api/v1/products');
+const salesRoute = require('./api/v1/sales');
 
 router.use('/', homeRoute);
 router.use('/api/v1/users', usersRoute);
 router.use('/api/v1/products', productsRoute);
+router.use('/api/v1/sales', salesRoute);
 
 
 module.exports = router;
