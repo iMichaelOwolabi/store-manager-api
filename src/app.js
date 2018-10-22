ddf5cd50bb689fa08c86639f5ec4d75b913c5b6b
Michael Owolabi <michael.i.owolabi@gmail.com>
2018-10-22 12:36:37 +0100

    Add a dynamic port for heroku

--- src/app.js
+++ src/app.js
@@ -10,14 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
 
-
-/*const server = app.listen(3000, ()=> {
-	console.log('The app is running successfully on port', server.address().port);
-});*/
-
-
 let port = process.env.PORT || (process.argv[2] || 3000);
-port = (typeof port === 'number') ? port : 3000;
 
 if(!module.parent){ app.listen(port); }
 
